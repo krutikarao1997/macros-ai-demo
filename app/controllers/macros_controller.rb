bc265e9ebe4eb317def34708e31a2ed938fd11a4
krutikarao1997 <krutikarao@chicagobooth.edu>
2025-04-29 23:42:16 +0000

    post working

--- app/controllers/macros_controller.rb
+++ app/controllers/macros_controller.rb
@@ -2,4 +2,8 @@ class MacrosController < ApplicationController
   def display_form     
     render({ :template => "macro_templates/new_form" })   
   end 
+
+  def do_magic
+    render({ :template => "macro_templates/results" })
+  end
 end
